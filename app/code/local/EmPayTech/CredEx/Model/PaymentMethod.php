37eec3eae4a646543decc92c7085e5cf24c2a25b
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-11-30 20:31:22 +0100

    remove whitespace

--- app/code/local/EmPayTech/CredEx/Model/PaymentMethod.php
+++ app/code/local/EmPayTech/CredEx/Model/PaymentMethod.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 /**
 * Our test CC module adapter
 */
@@ -11,7 +11,7 @@ class EmPayTech_CredEx_Model_PaymentMethod extends Mage_Payment_Model_Method_Abs
     * @var string [a-z0-9_]
     */
     protected $_code = 'credex';
- 
+
     /**
      * Here are examples of flags that will determine functionality availability
      * of this module to be used by frontend and backend.
@@ -21,57 +21,57 @@ class EmPayTech_CredEx_Model_PaymentMethod extends Mage_Payment_Model_Method_Abs
      * It is possible to have a custom dynamic logic by overloading
      * public function can* for each flag respectively
      */
-     
+
     /**
      * Is this payment method a gateway (online auth/charge) ?
      */
     protected $_isGateway               = true;
- 
+
     /**
      * Can authorize online?
      */
     protected $_canAuthorize            = true;
- 
+
     /**
      * Can capture funds online?
      */
     protected $_canCapture              = true;
- 
+
     /**
      * Can capture partial amounts online?
      */
     protected $_canCapturePartial       = false;
- 
+
     /**
      * Can refund online?
      */
     protected $_canRefund               = false;
- 
+
     /**
      * Can void transactions online?
      */
     protected $_canVoid                 = true;
- 
+
     /**
      * Can use this payment method in administration panel?
      */
     protected $_canUseInternal          = true;
- 
+
     /**
      * Can show this payment method as an option on checkout payment page?
      */
     protected $_canUseCheckout          = true;
- 
+
     /**
      * Is this payment method suitable for multi-shipping checkout?
      */
     protected $_canUseForMultishipping  = true;
- 
+
     /**
      * Can save credit card information for future processing?
      */
     protected $_canSaveCc = false;
- 
+
     /**
      * Here you will need to implement authorize, capture and void public methods
      *
