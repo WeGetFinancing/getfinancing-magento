0f99713220abdf5fa1b70843d29fcf8a89316652
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-12-07 21:11:53 +0100

    add our own session namespace

---
+++ app/code/local/EmPayTech/CredEx/Model/Session.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * EmPayTech CredEx Module
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   EmPayTech
+ * @package    EmPayTech_CredEx
+ * @copyright  Copyright (c) 2012 EmPayTech
+ * @author     Thomas Vander Stichele / EmPayTech <support@empaytech.net>
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ *
+ * Credex transaction session namespace
+ */
+
+class Mage_CredEx_Model_Session extends Mage_Core_Model_Session_Abstract
+{
+    public function __construct()
+    {
+        $this->init('credex');
+    }
+}
