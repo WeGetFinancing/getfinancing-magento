6d7274baa41561a88fb27b4777562fe49dc5739c
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-12-15 23:13:10 +0100

    add a library to get loan requests

---
+++ lib/EmPayTech/CredEx.php
@@ -0,0 +1,193 @@
+<?php
+/**
+ * EmPayTech CredEx module
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   EmPayTech
+ * @package    EmPayTech_CredEx
+ * @copyright  Copyright (c) 2012 EmPayTech
+ * @author     Thomas Vander Stichele / EmPayTech <support@empaytech.net>
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class CredEx
+{
+    public function __construct($url, $merch_id, $username, $password) {
+        $this->_url = $url;
+        $this->_merch_id = $merch_id;
+        $this->_username = $username;
+        $this->_password = $password;
+    }
+
+    public function log($msg) {
+    }
+
+    function request($fields) {
+        $fields = array_merge($fields, array(
+            'dg_username'=> $this->_username,
+            'dg_password'=> $this->_password,
+            'merch_id'=> $this->_merch_id));
+
+        $query = http_build_query($fields);
+        $this->log("request: $query");
+
+        $ch = curl_init($this->_url);
+
+        curl_setopt($ch, CURLOPT_URL, $this->_url);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION ,1);
+        // FIXME: we have to set response_format
+        //curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        //    "Accept: application/xml"
+        //));
+//        curl_setopt($ch, CURLOPT_HEADER, 0); // DO NOT RETURN HTTP HEADERS
+        /* return contents of the call as a variable */
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        $response = $this->parseResponse($result);
+
+        $this->log('response: ' . $response->asXML());
+        return $response;
+    }
+
+    public function parseResponse($xmlString)
+    {
+        /* SimpleXMLElement returns an element proxying for the root <response>
+           tag */
+        $response = new SimpleXMLElement($xmlString);
+
+        return $response;
+    }
+
+}
+
+class CredEx_Magento extends CredEx
+{
+    public function __construct($payment) {
+
+        $errors = FALSE;
+
+        $keys = array("platform", "merch_id", "username", "password");
+
+        foreach ($keys as $key) {
+            $$key = $payment->getConfigData($key);
+            if (!$$key) {
+                $this->log("specify $key in the configuration");
+                $errors = TRUE;
+            }
+        }
+
+        $platforms = array('staging', 'platform');
+        if (!in_array($platform, $platforms)) {
+            $this->log("platform is set to unsupported value $platform");
+            $errors = TRUE;
+        }
+
+        if ($errors) $this->misconfigured();
+
+        $key = "gateway_url_$platform";
+        $url = $payment->getConfigData($key);
+        if (!$url) {
+            Mage::log("Credex: specify $key in the configuration");
+            $errors = TRUE;
+        }
+
+        if ($errors) $this->misconfigured();
+
+        parent::__construct($url, $merch_id, $username, $password);
+    }
+
+    /* parent class implementations */
+    public function log($msg) {
+        Mage::log("Credex: $msg");
+    }
+
+    private function misconfigured()
+    {
+        Mage::throwException(Mage::helper('paygate')->__('The web store has misconfigured the Credex payment module.'));
+    }
+
+    /*
+     * Mage_Sales_Model_Quote
+     */
+    public function request($quote)
+    {
+        $errors = FALSE;
+
+        $billingaddress = $quote->getBillingAddress();
+        $shippingaddress = $quote->getShippingAddress();
+        $totals = number_format($quote->getGrandTotal(), 2, '.', '');
+        $this->log("request for total amount $totals");
+
+        $email = $quote->getCustomerEmail();
+        $fields = array(
+            'cust_fname'=> $billingaddress->getData('firstname'),
+            'cust_lname'=> $billingaddress->getData('lastname'),
+            'cust_email'=> $email,
+
+            'bill_addr_address'=> $billingaddress->getData('street'),
+            'bill_addr_city'=> $billingaddress->getData('city'),
+            'bill_addr_state'=> $billingaddress->getData('region'),
+            'bill_addr_zip'=> $billingaddress->getData('postcode'),
+
+            'ship_addr_address'=> $shippingaddress->getData('street'),
+            'ship_addr_city'=> $shippingaddress->getData('city'),
+            'ship_addr_state'=> $shippingaddress->getData('region'),
+            'ship_addr_zip'=> $shippingaddress->getData('postcode'),
+
+            'version' => '0.3',
+            'response_format' => 'XML',
+            // FIXME
+            'cust_id_ext'=> $quote->getIncrementId(),
+            'inv_action' => 'AUTH_ONLY',
+            'inv_value' => $totals,
+            /* FIXME: description */
+            'udf02' => 'Sample purchase'
+        );
+
+        $response = parent::request($fields);
+
+        if ($response->status_code == "GW_NOT_AUTH") {
+            Mage::log("Credex: " . $response->status_string);
+            Mage::log("Credex: Please verify your authentication details.");
+            $this->misconfigured();
+        }
+
+        if ($response->status_code == "APPROVED") {
+            $this->getSession()->setApplicationURL((string) $response->udf02);
+            $this->getSession()->setCustId((string) $response->cust_id);
+            $this->getSession()->setInvId((string) $response->inv_id);
+        } else {
+            /* throwing an exception makes us stay on this page so we can repeat */
+            Mage::throwException(Mage::helper('paygate')->__('Payment capturing error.'));
+        }
+
+        return $response;
+    }
+
+     /**
+     * Get credex session namespace
+     *
+     * @return Mage_Credex_Model_Session
+     */
+    public function getSession()
+    {
+        return Mage::getSingleton('checkout/session');
+    }
+
+
+}
