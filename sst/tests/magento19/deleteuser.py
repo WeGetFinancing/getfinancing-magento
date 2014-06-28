502a1f060aaad6b3662c474f0ea5a03b8252e534
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2014-06-27 20:02:42 -0400

    make deleteuser work

--- sst/tests/magento19/deleteuser.py
+++ sst/tests/magento19/deleteuser.py
@@ -1,28 +1,36 @@
 # -*- Mode: Python -*-
 # vi:si:et:sw=4:sts=4:ts=4
 
-from sst.actions import *
+from sst import actions as a
 
 import common
 import data
 
-go_to('http://magento19.localhost/admin/')
-assert_title_contains('Admin')
+a.go_to('http://magento19.localhost/admin/')
+a.assert_title_contains('Admin')
 
 common.fill_user_form(data.admin)
-button = get_element_by_xpath("//input[@title='Login']")
-click_button(button)
+button = a.get_element_by_xpath("//input[@title='Login']")
+a.click_button(button)
 
 # do away with messages if any
 try:
-    close = get_element_by_xpath(
+    close = a.get_element_by_xpath(
         "//div[@id='message-popup-window']"
         "//a[@title='close']")
 except AssertionError:
     pass
 if close:
-    click_link(close)
+    a.click_link(close)
 
 common.admin_nav('Customers', 'Manage Customers')
+common.customer_edit_by_email(data.user['email_address'][1])
 
-raise
+# for some reason there are two buttons? I can only find one in the source
+buttons = a.get_elements(title='Delete Customer')
+# wait=False lets us interact with the alert
+a.click_button(buttons[0], wait=False)
+# confirm the dialog when clicking to delete
+a.accept_alert('Are you sure you want to do this?')
+
+a.click_link(a.get_element(text='Log Out'))
--- sst/tests/shared/common.py
+++ sst/tests/shared/common.py
@@ -20,16 +20,33 @@ def fill_user_form(user):
 def admin_nav(first, second):
     # no idea how to hover, but there's a false click handler so that works
     hover_link = a.get_element_by_xpath(
-        "//li[%s]/*/*[text()[contains(.,'%s')]]/.." % (
-            xpath_contains_class('level0'), first))
+        "//li[%s]/*/*[%s]/.." % (
+            xpath_contains_class('level0'),
+            xpath_contains_text(first))
+    )
     a.click_link(hover_link)
 
     final_link = a.get_element_by_xpath(
-        "//li[%s]/*/*[text()[contains(.,'%s')]]/.." % (
-            xpath_contains_class('level1'), second))
+        "//li[%s]/*/*[%s]/.." % (
+            xpath_contains_class('level1'),
+            xpath_contains_text(second))
+    )
     a.click_link(final_link)
+
+def customer_edit_by_email(email):
+    edit_link = a.get_element_by_xpath(
+        "//table[@id='customerGrid_table']"
+        "//td[%s]"
+        "//.."
+        "//a" % (xpath_contains_text(email))
+    )
+    a.click_link(edit_link)
+
 
 # See
 # http://stackoverflow.com/questions/8808921/selecting-a-css-class-with-xpath
 def xpath_contains_class(name):
     return 'contains(concat(" ", normalize-space(@class), " "), " %s ")' % name
+
+def xpath_contains_text(text):
+    return "text()[contains(.,'%s')]" % text
